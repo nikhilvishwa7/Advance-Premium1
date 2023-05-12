fecd995ac581d72ed555fcb4698545af83072e47
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2023-05-12 23:23:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1601,7 +1601,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
          reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.HELPZ_TXT,
+            text=script.HELPD_TXT,
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )    
