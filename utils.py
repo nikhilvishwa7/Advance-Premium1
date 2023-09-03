cd499cc32b3d1d82d433ad7e4f7411e59b244d6d
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-09-03 10:03:31 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ import re
 import os
 from datetime import datetime, date
 import string
-from typing import List
+from typing import List, Any
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
@@ -632,3 +632,25 @@ async def check_verification(bot, userid):
             return True
     else:
         return False
+
+#online streaming & fast download features
+def get_media_from_message(message: "Message") -> Any:
+    media_types = (
+        "audio",
+        "document",
+        "photo",
+        "sticker",
+        "animation",
+        "video",
+        "voice",
+        "video_note",
+    )
+    for attr in media_types:
+        media = getattr(message, attr, None)
+        if media:
+            return media
+
+
+def get_hash(media_msg: Message) -> str:
+    media = get_media_from_message(media_msg)
+    return getattr(media, "file_unique_id", "")[:6]
