ad0cbbab814871cb077fda58fce83029ef5bbfc6
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-10-13 00:13:33 -0700

    Update Nolink.py

--- plugins/local_plugins/Nolink.py
+++ plugins/local_plugins/Nolink.py
@@ -1,27 +1,42 @@
 import os 
 import pyrogram
-from info import ADMINS
+from pyrogram import Client, filters
+from info import BOT_TOKEN, API_ID, API_HASH
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
 import asyncio
-from pyrogram import Client, filters, enums
-from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-import time
-from datetime import datetime
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-import logging
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
 
-@Client.on_message((filters.group) & (filters.regex("@")  | filters.regex("t.me") | filters.regex("https")))
+
+
+Bot = Client(
+    "NoLink-BOT",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+
+        
+
+
+@Client.on_message(filters.regex("http") & filters.regex("www") | filters.regex("@") | filters.regex("https") | filters.regex("t.me") & filters.group)
 async def nolink(bot,message):
-	if message.from_user.id in ADMINS:
-		return
-
+    
 	try:
-              
-                aks = await message.reply_text("<b>are you mad??\nwhy you send link in group 🤬</b>")
-                await asyncio.sleep(60)      
-                k = await aks.delete()
-                await message.delete()
+                
+                buttons = [[
+                    InlineKeyboardButton('sᴜʀᴘʀɪsᴇ', url='{content}')
+                ]]
+                reply_markup = InlineKeyboardMarkup(buttons)
+                await message.reply_sticker("CAACAgUAAx0CXPjPGAACAmVkAAHLpxQlUkQIctGPhN_l36xk9psAAlcJAAKTvwlU-kg3cws4x6geBA") 
+                        
+                
+                hmm = await message.delete()
+                return
+                
+
+
 	except:
 		return
+        
