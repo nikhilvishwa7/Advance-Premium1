2626962784fd4c13dc4af4c8ec989923162182cc
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-11-04 07:14:57 -0700

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -11,8 +11,8 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = []
+CHANNELS = []
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
@@ -20,5 +20,3 @@ AUTH_CHANNEL = None
 DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
-
-
