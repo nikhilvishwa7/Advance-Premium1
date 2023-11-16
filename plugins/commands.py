d638579e76a8b7a61be30ed26af2360e7b228718
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-11-16 05:55:04 -0800

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -932,7 +932,7 @@ async def send_msg(bot, message):
     else:
         await message.reply_text("<b>Use this command as a reply to any message using the target chat id. For eg: /send userid</b>")
 
-@Client.on_message(filters.command("deletethis") & filters.user(ADMINS))
+@Client.on_message(filters.command("deletefiles") & filters.user(ADMINS))
 async def deletemultiplefiles(bot, message):
     chat_type = message.chat.type
     if chat_type != enums.ChatType.PRIVATE:
