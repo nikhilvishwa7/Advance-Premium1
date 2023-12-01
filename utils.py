d6ed04c545e238c19524f4e7fe268841479d5b7a
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2023-11-30 22:58:55 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -630,6 +630,40 @@ async def check_verification(bot, userid):
             return True
     else:
         return False
+
+async def get_seconds(time_string):
+    def extract_value_and_unit(ts):
+        value = ""
+        unit = ""
+
+        index = 0
+        while index < len(ts) and ts[index].isdigit():
+            value += ts[index]
+            index += 1
+
+        unit = ts[index:].lstrip()
+
+        if value:
+            value = int(value)
+
+        return value, unit
+
+    value, unit = extract_value_and_unit(time_string)
+
+    if unit == 's':
+        return value
+    elif unit == 'min':
+        return value * 60
+    elif unit == 'hour':
+        return value * 3600
+    elif unit == 'day':
+        return value * 86400
+    elif unit == 'month':
+        return value * 86400 * 30
+    elif unit == 'year':
+        return value * 86400 * 365
+    else:
+        return 0
     
     
 async def send_all(bot, userid, files, ident, chat_id, user_name, query):
