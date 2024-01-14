b89fb211230a3533901cd7c695a9819de658d7d4
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-01-13 20:29:15 -0800

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -1,5 +1,7 @@
 from pyrogram import Client, filters, enums
 import psutil
+from pytz import timezone
+from datetime import datetime
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import ADMINS, LOG_CHANNEL, SUPPORT_CHAT, MELCOW_NEW_USERS, MELCOW_VID, CHNL_LNK, GRP_LNK, GROUP_LOGS
