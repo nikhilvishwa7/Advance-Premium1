1a025c0197e6a2cef44c72f615da89322172c8c2
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2024-08-18 16:29:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -52,7 +52,6 @@ class script(object):
 
     ABOUT_TXT = """<b>
  🤖 ᴍʏ ɴᴀᴍᴇ : {}
- 👨‍💻 ᴄʀᴇᴀᴛᴏʀ : <a href='https://t.me/RyZer09'>RyZeR</a>
  📚 Owner : <a href='https://t.me/Abhikumar7799'>Owner</a>
  🥶 ʙᴜɪʟᴅ sᴛᴀᴛᴜs : ᴠ3.0 [sᴛᴀʙʟᴇ​]</b>"""
     
