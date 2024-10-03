8021b0ccacf8992ffb9e3f49d261dd2bf8b32f9b
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2024-10-03 13:31:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5
 STICKERS = (environ.get('STICKERS', 'CAACAgIAAxkBAAELyK1mAAFuOcDG7bGYUECxCDkY0nCzLUMAAh4JAAIYQu4I-VjZ7h0hnCE0BA')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5846960534 5510862070').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5149183428 5846960534').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
